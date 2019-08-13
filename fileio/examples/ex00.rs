f499a705b730da8803edd6dcd548f56c207a1c2e
Michael Angerman <stormasm@gmail.com>
2019-08-13 09:18:15 -0700

    if the num of arguments is not 2 then bail

--- fileio/examples/ex00.rs
+++ fileio/examples/ex00.rs
@@ -1,7 +1,12 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("You need to enter a filename");
+        process::exit(1);
+    }
     let filename = &args[1];
     println!("In file {}", filename);
 }
