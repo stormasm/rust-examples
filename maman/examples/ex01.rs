b640c0e7b1cfdbc865e487310a3dfaa5b44a68b6
Michael Angerman <stormasm@gmail.com>
2019-09-26 14:57:07 -0700

    change index from 1 to 0

--- maman/examples/ex01.rs
+++ maman/examples/ex01.rs
@@ -4,7 +4,7 @@ fn fetch_a_string() -> redis::RedisResult<String> {
     // connect to redis
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection()?;
-    con.lindex("development:queue:maman",1)
+    con.lindex("development:queue:maman",0)
 }
 
 fn main() {
