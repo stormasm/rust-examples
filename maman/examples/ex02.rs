6eea140ec62abac71720844b992577cb2123424e
Michael Angerman <stormasm@gmail.com>
2019-09-27 13:12:36 -0700

    delete ex03 and move it to ex02

--- maman/examples/ex02.rs
+++ maman/examples/ex02.rs
@@ -1,9 +1,13 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate redis;
+extern crate serde;
+extern crate serde_json;
 use redis::Commands;
-use redis::FromRedisValue;
-// use serde::{Serialize, Serializer};
-use serde;
+
 use serde_json::value::Value;
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Job {
     pub class: String,
     pub args: Vec<Value>,
@@ -14,27 +18,20 @@ pub struct Job {
     pub enqueued_at: u64,
 }
 
-fn fetch_json() -> redis::RedisResult<String> {
-    // connect to redis
+fn do_something() -> redis::RedisResult<()> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection()?;
-    let mut p1 = from_redis_value(con.lindex("development:queue:maman",0));
 
-    let mut p2: Job = serde_json::from_str(p1).unwrap();
-    println!("{:?}", p2);
-    con.lindex("development:queue:maman",0)
-}
+    let k: Option<String> = con.lindex("development:queue:maman", 0)?;
+    let k1 = k.unwrap();
 
-fn fetch_a_string() -> redis::RedisResult<String> {
-    // connect to redis
-    let client = redis::Client::open("redis://127.0.0.1/")?;
-    let mut con = client.get_connection()?;
-    con.lindex("development:queue:maman",0)
+    let deserialized: Job = serde_json::from_str(&k1).unwrap();
+    println!("Deserialized: {:?}", deserialized);
+
+    Ok(())
 }
 
 fn main() {
     println!("Hello, redis!");
-    let _y = fetch_a_string();
-    println!("{:?}",_y);
-    fetch_json();
+    do_something();
 }
