5608c4e8ce9cb8ec4fd2137dacbe5c5054c0708e
Michael Angerman <stormasm@gmail.com>
2019-09-27 09:50:17 -0700

    writing and reading json from redis is now officially working !

--- serde/examples/ex01.rs
+++ serde/examples/ex01.rs
@@ -1,30 +1,34 @@
 #[macro_use]
 extern crate serde_derive;
+extern crate redis;
 extern crate serde;
 extern crate serde_json;
-extern crate redis;
 use redis::Commands;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Contact {
     firstname: String,
-    lastname: String
+    lastname: String,
 }
-
 
 fn do_something() -> redis::RedisResult<()> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection()?;
 
-    let mycontact = Contact { firstname: "rick".to_string(), lastname: "stevens".to_string() };
+    let mycontact = Contact {
+        firstname: "rick".to_string(),
+        lastname: "stevens".to_string(),
+    };
     let serialized = serde_json::to_string(&mycontact).unwrap();
     println!("Serialized: {}", serialized);
     con.set("rick", serialized)?;
-    let json_from_redis = con.get("rick").to_string();
+    //let json_from_redis = con.get("rick").to_string();
 
-    let deserialized: Contact = serde_json::from_str(&json_from_redis).unwrap();
+    let k: Option<String> = con.get("rick")?;
+    let k1 = k.unwrap();
+
+    let deserialized: Contact = serde_json::from_str(&k1).unwrap();
     println!("Deserialized: {:?}", deserialized);
-
 
     Ok(())
 }
