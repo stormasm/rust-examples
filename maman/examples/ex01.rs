67005e7e5ff59c1317283b42d981828d0e53cec1
Michael Angerman <stormasm@gmail.com>
2019-09-27 13:13:36 -0700

    rf

--- maman/examples/ex01.rs
+++ maman/examples/ex01.rs
@@ -4,11 +4,11 @@ fn fetch_a_string() -> redis::RedisResult<String> {
     // connect to redis
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection()?;
-    con.lindex("development:queue:maman",0)
+    con.lindex("development:queue:maman", 0)
 }
 
 fn main() {
     println!("Hello, redis!");
     let _y = fetch_a_string();
-    println!("{:?}",_y)
+    println!("{:?}", _y)
 }
