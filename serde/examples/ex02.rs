d2248293e0c04eab606eea44eb930b50eed55c2a
Michael Angerman <stormasm@gmail.com>
2019-10-09 18:41:31 -0700

    mw

--- serde/examples/ex02.rs
+++ serde/examples/ex02.rs
@@ -8,20 +8,18 @@ use serde_json::{Result, Value};
 fn t1() -> Result<()> {
     // Some JSON input data as a &str. Maybe this comes from the user.
     let data = r#"
-        {
-            "name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
+
+            [
+                "abc",
+                "def"
             ]
-        }"#;
+        "#;
 
     // Parse the string of data into serde_json::Value.
     let v: Value = serde_json::from_str(data)?;
 
     // Access parts of the data by indexing with square brackets.
-    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
+    println!("hola {}", v);
 
     Ok(())
 }
