8ae1701a0021a2d4e917ed788bf5996f6c58a93f
Michael Angerman <stormasm@gmail.com>
2019-10-11 09:53:55 -0700

    converting serde json to a vector

---
+++ serde/examples/ex03.rs
@@ -0,0 +1,37 @@
+use serde_json::{Result, Value};
+
+fn t1() -> Result<()> {
+    let data = r#"
+            [
+                "abc",
+                "def",
+                "ghi"
+            ]
+        "#;
+
+    let v: Value = serde_json::from_str(data)?;
+
+    let vec = v.as_array().unwrap();
+
+    println!("len = {}", vec.len());
+
+    for i in 0..vec.len() {
+        println!("{}", vec[i]);
+
+        let vx = &vec[i].as_str().unwrap();
+        println!("{}", vx);
+    }
+
+    // Access parts of the data by indexing with square brackets.
+    println!("with quotes {}", v[0]);
+
+    let v1 = &v[0].as_str().unwrap();
+    println!("without quotes = {}", v1);
+
+    Ok(())
+}
+
+fn main() {
+    println!("Hello, Bill!");
+    let _v = t1();
+}
