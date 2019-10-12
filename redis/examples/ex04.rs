55bcfcb070acd67318f43bc03636d366822704c0
Michael Angerman <stormasm@gmail.com>
2019-10-11 20:46:22 -0700

    starting to work on ex04

---
+++ redis/examples/ex04.rs
@@ -0,0 +1,59 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::string::String;
+
+use redis::Commands;
+
+use serde_json::{Result, Value};
+
+
+use serde_json::json;
+
+fn string_to_json() -> Result<(Value)> {
+
+    let data = json!(
+        ["21195107","21190487","21189256","21193497","21191588"]
+    );
+
+    let value = json!([
+        "notice",
+        "the",
+        "trailing",
+        "comma -->",
+    ]);
+
+    // Parse the string of data into serde_json::Value.
+    //let v: Value = serde_json::from_str(&value);
+    Ok(data)
+}
+
+fn write_json_to_redis(json: Value) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    // Convert the serde value to a Vector
+    let vec = json.as_array().unwrap();
+
+    // iterate over the vector
+    for i in 0..vec.len() {
+        // you must convert &str to String
+        let vy = &vec[i].as_str().unwrap().to_string();
+        redis::cmd("SADD").arg("bill").arg(vy).execute(&mut con);
+    }
+
+    let myid = String::from("999");
+    con.set("rick", myid)?;
+
+    let k: Option<String> = con.get("rick")?;
+    let k1 = k.unwrap();
+    redis::cmd("SADD").arg("pete").arg(k1).execute(&mut con);
+
+    let x55 = String::from("55");
+    redis::cmd("SADD").arg("pete").arg(x55).execute(&mut con);
+    Ok(())
+}
+
+fn main() {
+    let json = string_to_json();
+    let _x = write_json_to_redis(json);
+}
