e30cbddcb4c1164a5bc952e8e2820fb5d705b69a
Michael Angerman <stormasm@gmail.com>
2019-10-12 12:58:06 -0700

    first cut at working dir reader

---
+++ fileio/examples/ex02.rs
@@ -0,0 +1,39 @@
+use std::{env, fs};
+// use std::io::Error;
+use std::error::Error;
+
+//use core::fmt::Error;
+
+//use std::fmt::Error;
+
+fn dir_reader() -> Result<(), Box<Error>> {
+    let current_dir = env::current_dir()?;
+    println!(
+        "Entries modified in the last 24 hours in {:?}:",
+        current_dir
+    );
+
+    for entry in fs::read_dir(current_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        let metadata = fs::metadata(&path)?;
+        let last_modified = metadata.modified()?.elapsed()?.as_secs();
+
+        if last_modified < 24 * 3600 && metadata.is_file() {
+            println!(
+                "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
+                last_modified,
+                metadata.permissions().readonly(),
+                metadata.len(),
+                path.file_name().ok_or("No filename")?
+            );
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    dir_reader();
+}
