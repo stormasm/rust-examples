5227f90ccc8d1ec90863f3950a052716c718f9aa
Michael Angerman <stormasm@gmail.com>
2019-10-12 11:57:18 -0700

    ex01

---
+++ fileio/examples/ex01.rs
@@ -0,0 +1,43 @@
+use std::env;
+// use std::fs;
+use std::process;
+use std::string::String;
+
+use std::fs::{self, DirEntry};
+use std::io;
+use std::path::Path;
+
+// one possible implementation of walking a directory only visiting files
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn read_dir_to_vector(filename: String) -> String {
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    contents
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("You need to enter a directory name");
+        process::exit(1);
+    }
+    let dirname = &args[1];
+    println!("In directory {}", dirname);
+
+    let contents = read_dir_to_vector(dirname.to_string());
+
+    println!("With text:\n{}", contents);
+}
