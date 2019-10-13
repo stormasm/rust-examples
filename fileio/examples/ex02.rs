acf0bd351587c8b3b971a1af1abc98cacbee067c
Michael Angerman <stormasm@gmail.com>
2019-10-12 21:33:10 -0700

    mw

--- fileio/examples/ex02.rs
+++ fileio/examples/ex02.rs
@@ -3,23 +3,15 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 fn dir_reader() -> Result<Vec<PathBuf>, Box<dyn Error>> {
-    // fn dir_reader() -> Result<Vec<&str>,_>  {
     let current_dir = Path::new("/tmp09/rust-hackernews/hn00/data/in");
     println!("Entries in {:?}:", current_dir);
 
     let mut vec: Vec<PathBuf> = Vec::new();
 
-    // let mut vec: Vec<&str> = vec![&str];
-
-    //    let vec: Vec<str> = Vec::new();
-    //    vec.resize(len, 0);
-
     for entry in fs::read_dir(current_dir)? {
         let entry = entry?;
         let path = entry.path();
-
-        println!("{:?}", path.file_name().ok_or("No filename")?);
-
+        //println!("{:?}", path.file_name().ok_or("No filename")?);
         vec.push(path);
     }
 
@@ -27,20 +19,11 @@ fn dir_reader() -> Result<Vec<PathBuf>, Box<dyn Error>> {
 }
 
 fn processor() -> Result<(), Box<dyn Error>> {
-    //fn processor() -> Result(_,_) {
     let vec = dir_reader().unwrap();
-
-    println!("vec len = {:?}", vec.len());
+    // println!("vec len = {:?}", vec.len());
 
     for name in vec {
-        // println!("{:?}", name.path.file_name().ok_or("No filename")?);
-        // println!("{:?}", name.capacity());
-
-        // let () = name;
-
         println!("{:?}", name.file_name().ok_or("No filename")?);
-
-        //println!("{:?}", name.file_name);
     }
 
     Ok(())
