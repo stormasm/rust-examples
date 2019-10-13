7d115a90dddf2d49c8e206ff0aa8157624b2e65a
Michael Angerman <stormasm@gmail.com>
2019-10-12 20:52:25 -0700

    ex02

--- fileio/examples/ex02.rs
+++ fileio/examples/ex02.rs
@@ -1,13 +1,18 @@
 use std::error::Error;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::{env, fs};
 
-fn dir_reader() -> Result<(), Box<Error>> {
+fn dir_reader() -> Result<Vec<PathBuf>, Box<dyn Error>> {
+    // fn dir_reader() -> Result<Vec<&str>,_>  {
     let current_dir = Path::new("/tmp09/rust-hackernews/hn00/data/in");
-    println!(
-        "Entries in {:?}:",
-        current_dir
-    );
+    println!("Entries in {:?}:", current_dir);
+
+    let mut vec: Vec<PathBuf> = Vec::new();
+
+    // let mut vec: Vec<&str> = vec![&str];
+
+    //    let vec: Vec<str> = Vec::new();
+    //    vec.resize(len, 0);
 
     for entry in fs::read_dir(current_dir)? {
         let entry = entry?;
@@ -15,23 +20,12 @@ fn dir_reader() -> Result<(), Box<Error>> {
 
         println!("{:?}", path.file_name().ok_or("No filename")?);
 
-        let metadata = fs::metadata(&path)?;
-        let last_modified = metadata.modified()?.elapsed()?.as_secs();
-
-        if last_modified < 24 * 3600 && metadata.is_file() {
-            println!(
-                "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
-                last_modified,
-                metadata.permissions().readonly(),
-                metadata.len(),
-                path.file_name().ok_or("No filename")?
-            );
-        }
+        vec.push(path);
     }
 
-    Ok(())
+    Ok(vec)
 }
 
 fn main() {
-    dir_reader();
+    let myvec = dir_reader();
 }
