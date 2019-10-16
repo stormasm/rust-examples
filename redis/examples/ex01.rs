48f5d3904a5d1ddee99c90d93477e0df3c8bd68c
Michael Angerman <stormasm@gmail.com>
2019-10-16 11:28:24 -0700

    ex01 update with redis hashmap stuff

--- redis/examples/ex01.rs
+++ redis/examples/ex01.rs
@@ -7,15 +7,24 @@ fn fetch_an_integer() -> redis::RedisResult<isize> {
     // let mut con = client.get_connection()?;
     let mut con = client.get_connection().expect("Failed to connect to Redis");
     // throw away the result, just make sure it does not fail
-    let _ : () = con.set("my_key120", 120)?;
+    let _: () = con.set("my_key120", 120)?;
     // read back the key and return it.  Because the return value
     // from the function is a result for integer this will automatically
     // convert into one.
+    //
+
+    let _: () = con.hset("hm-120", "58", "hola mike")?;
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+
+    // let x1 : redis::RedisResult = con.hget("hm-120","120");
+
     con.get("my_key120")
 }
 
 fn main() {
     println!("Hello, redis!");
     let _y = fetch_an_integer();
-    println!("{:?}",_y)
+    println!("{:?}", _y)
 }
