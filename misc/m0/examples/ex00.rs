c3d78d0952c2b9d35db338429885ca31732c92df
Michael Angerman <stormasm@gmail.com>
2019-10-18 08:47:09 -0700

    moved guessing_game here

---
+++ misc/m0/examples/ex00.rs
@@ -0,0 +1,37 @@
+fn main() {
+    println!("Hello, storm!");
+
+    use rand::Rng;
+    use std::cmp::Ordering;
+    use std::io;
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 5);
+
+    loop {
+        println!("Please input your guess between 1 and 5.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
