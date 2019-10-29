a33dd139cf0015a20f99f876cc024a5cff9b2234
Michael Angerman <stormasm@gmail.com>
2019-10-29 09:12:49 -0700

    deletedir update

--- fileio/examples/deletedir.rs
+++ fileio/examples/deletedir.rs
@@ -2,12 +2,12 @@ use std::fs::{create_dir, remove_dir_all};
 use std::path::Path;
 
 fn main() {
-    let check_path = Path::new("/tmp/tantivy");
+    let check_path = Path::new("/tmp/corvallis");
     let dir_exists = check_path.exists();
     if dir_exists {
         remove_dir_all(check_path).expect("dir does not exist");
     }
 
-    let index_path = Path::new("/tmp/tantivy");
+    let index_path = Path::new("/tmp/corvallis");
     create_dir(index_path).expect("dir already exists");
 }
