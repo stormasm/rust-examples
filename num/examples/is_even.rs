317ec4d637cf2447bdd42e4f1cf2f8736dbc7b72
Michael Angerman <stormasm@gmail.com>
2019-12-06 16:33:13 -0500

    is_even and is_odd are up and running

--- num/examples/is_even.rs
+++ num/examples/is_even.rs
@@ -1,6 +1,9 @@
-
 fn is_even(num: u32) -> bool {
     (num) & 1 == 0
+}
+
+fn is_odd(num: u32) -> bool {
+    !is_even(num)
 }
 
 /*
@@ -42,5 +45,10 @@ fn main() {
     let x1 = is_even(1);
     let x2 = is_even(2);
     let x3 = is_even(3);
-    println!("{} {} {} {}",x0,x1,x2,x3);
+    println!("{} {} {} {}", x0, x1, x2, x3);
+    let y0 = is_odd(0);
+    let y1 = is_odd(1);
+    let y2 = is_odd(2);
+    let y3 = is_odd(3);
+    println!("{} {} {} {}", y0, y1, y2, y3);
 }
