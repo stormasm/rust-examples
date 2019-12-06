701ed2bf7e423a55c7fd1d696549b7627ba792af
Michael Angerman <stormasm@gmail.com>
2019-12-06 16:34:54 -0500

    cc

--- num/examples/is_even.rs
+++ num/examples/is_even.rs
@@ -1,3 +1,5 @@
+// https://github.com/rust-num/num-integer
+
 fn is_even(num: u32) -> bool {
     (num) & 1 == 0
 }
@@ -5,40 +7,6 @@ fn is_even(num: u32) -> bool {
 fn is_odd(num: u32) -> bool {
     !is_even(num)
 }
-
-/*
-fn is_odd(self) -> bool {
-    !self.is_even()
-}
-*/
-
-/*
-fn test_even() {
-    assert_eq!((-4).is_even(), true);
-    assert_eq!((-3).is_even(), false);
-    assert_eq!((-2).is_even(), true);
-    assert_eq!((-1).is_even(), false);
-    assert_eq!((0 .is_even(), true);
-    assert_eq!((1).is_even(), false);
-    assert_eq!((2).is_even(), true);
-    assert_eq!((3).is_even(), false);
-    assert_eq!((4).is_even(), true);
-}
-*/
-
-/*
-fn test_odd() {
-    assert_eq!((-4 as $T).is_odd(), false);
-    assert_eq!((-3 as $T).is_odd(), true);
-    assert_eq!((-2 as $T).is_odd(), false);
-    assert_eq!((-1 as $T).is_odd(), true);
-    assert_eq!((0 as $T).is_odd(), false);
-    assert_eq!((1 as $T).is_odd(), true);
-    assert_eq!((2 as $T).is_odd(), false);
-    assert_eq!((3 as $T).is_odd(), true);
-    assert_eq!((4 as $T).is_odd(), false);
-}
-*/
 
 fn main() {
     let x0 = is_even(0);
