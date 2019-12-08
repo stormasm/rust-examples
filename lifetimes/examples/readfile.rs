245c80275f70981e2afead9878a5f35ac3208279
Michael Angerman <stormasm@gmail.com>
2019-12-08 14:02:35 -0500

    readfile

--- lifetimes/examples/readfile.rs
+++ lifetimes/examples/readfile.rs
@@ -26,14 +26,20 @@ fn readfile(filename: String) {
     //
     let mut writer = BufWriter::new(io::stdout());
     for (num, line) in file.lines().enumerate() {
-        let mynum = num;
         let myline = line.unwrap().clone();
-        println!("{}", myline);
+        /*
+                let mynum = num;
+                let myline = line.unwrap().clone();
+                println!("{}", myline);
+        */
         if is_even(num.try_into().unwrap()) {
-            writeln!(writer, "{0}\n", mynum).unwrap();
+            writeln!(writer, "{0}\n", num).unwrap();
+            writeln!(writer, "{0}\n", myline).unwrap();
             // vec_key.push(&myline);
         }
         if !is_even(num.try_into().unwrap()) {
+            writeln!(writer, "{0}\n", num).unwrap();
+            writeln!(writer, "{0}\n", myline).unwrap();
             // vec_value.push(&myline);
         }
     }
