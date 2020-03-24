18a0502a407840bafca2cac924e42938beab4f58
Michael Angerman <stormasm@gmail.com>
2020-03-24 14:01:44 -0700

    mw

---
+++ format/examples/ex04.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+
+use std::fmt::Write as FmtWrite;
+//use std::io::Write as IoWrite;
+
+#[derive(Debug)]
+struct Point {
+    measurement: String,
+    timestamp: String,
+    fieldset: HashMap<String, String>,
+    tagset: HashMap<String, String>,
+}
+
+impl Point {
+    fn set_fieldset(volume: String, close: String) -> HashMap<String, String> {
+        let mut foo = HashMap::new();
+        foo.insert("volume".to_string(), volume);
+        foo.insert("close".to_string(), close);
+        foo.clone()
+    }
+
+    fn set_tagset() -> HashMap<String, String> {
+        let mut foo = HashMap::new();
+        foo.insert("frequency".to_string(), "daily".to_string());
+        foo.insert("type".to_string(), "close".to_string());
+        foo.clone()
+    }
+
+    fn get_tagset(self) -> Result<String, Box<dyn std::error::Error>> {
+        //    fn get_fieldset(self) -> String {
+        let mut s = String::new();
+        write!(&mut s, "{},", self.measurement).expect("E0");
+        for (key, val) in self.tagset {
+            write!(&mut s, "{}={},", key, val).expect("E1");
+        }
+        let mut strlen = s.len();
+        let mut s1 = String::from(s);
+        s1.remove(strlen - 1);
+
+        write!(&mut s1, "{}", " ".to_string()).expect("E2");
+
+        for (key, val) in self.fieldset {
+            write!(&mut s1, "{}={},", key, val).expect("E3");
+        }
+
+        strlen = s1.len();
+        let mut s2 = String::from(s1);
+        s2.remove(strlen - 1);
+
+
+
+        println!("{}", s2);
+        Ok(s2)
+    }
+}
+fn main() {
+    let point: Point = Point {
+        measurement: "ui".to_string(),
+        timestamp: "1583712000".to_string(),
+        fieldset: Point::set_fieldset("348000.00".to_string(), "127.21".to_string()),
+        tagset: Point::set_tagset(),
+    };
+    let x = point.get_tagset().unwrap();
+    println!("{:?}", x);
+}
