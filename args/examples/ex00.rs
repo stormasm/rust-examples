8676a4afcc197f7f9472dbef8cc0447c8eaf95ff
Michael Angerman <stormasm@gmail.com>
2020-08-16 21:34:18 -0700

    mw

--- args/examples/ex00.rs
+++ args/examples/ex00.rs
@@ -2,13 +2,13 @@ fn main() {
     let args: Vec<_> = std::env::args().collect();
 
 //  Figure out how to pass in the correct value
-//  let width = args[1].parse::<usize>().expect("Need a width in columns");
-    let width = 20000;
+    let width = args[1].parse::<usize>().expect("Need a width in columns");
+//  let width = 20000;
     process(width);
 }
 
 fn process(width: usize) {
-    assert_eq!(width, 20000);
+    assert_eq!(width, 2);
 }
 
 // pub fn draw_table(table: &Table, termwidth: usize) {
