77e9e50502096ff006f6809c0c80729064a9337c
Michael Angerman <stormasm@gmail.com>
2020-09-06 10:22:52 -0700

    mw

---
+++ ownership/examples/bookmut.rs
@@ -0,0 +1,31 @@
+// This is an example from Yehuda Katz's excellent talk on Rust
+// https://www.youtube.com/watch?v=uCaYkUmdtPw
+
+pub struct Book {
+    title: String
+}
+
+fn main() {
+    let title = "Gone with the wind".to_string();
+    let title3 = "Sams lunch box".to_string();
+    let book = Book {title};
+    //let title2 = "Sam's Lunch Box".to_string();
+    //let mut book2 = Book {"Sams Lunch Box"};
+    let mut book3 = Book {title3};
+    //title_book(&mut book3,"Bills Tea Cup");
+    print_book(&book);
+    println!("{}",book.title);
+    //println!("{}",book2.title);
+}
+
+fn print_book(book: &Book) {
+    println!("Title: {}",book.title);
+}
+
+fn change_title(book: &mut Book) {
+    book.title = "Rat Race".to_string();
+}
+
+fn title_book(book: &mut Book, title: &str) {
+    book.title = title.to_string();
+}
