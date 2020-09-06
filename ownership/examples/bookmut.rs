23aa2888baa49a47542c6f65717c6b1ff0fb6d89
Michael Angerman <stormasm@gmail.com>
2020-09-06 12:56:37 -0700

    mw

--- ownership/examples/bookmut.rs
+++ ownership/examples/bookmut.rs
@@ -12,23 +12,23 @@ fn main() {
     let book1 = Book {title};
     let book2 = Book {title: title2};
     let mut book3 = Book {title: "Sams Lunch Box".to_string()};
-    print_book(&book1);
-    print_book(&book2);
-    println!("Book 3 title = {}",book3.title);
+    print_book("Book 1", &book1);
+    print_book("Book 2", &book2);
+    println!("Book 3 Title: {}",book3.title);
     change_title(&mut book3);
-    println!("Book 3 title = {}",book3.title);
-    print_book(&book3);
+    println!("Book 3 Title: {}",book3.title);
+    print_book("Book 3", &book3);
     let mut book4 = Book {title: "".to_string()};
     title_book(&mut book4,"Bills Tea Cup");
-    print_book(&book4);
+    print_book("Book 4", &book4);
     title_book(&mut book4,title4);
-    print_book(&book4);
+    print_book("Book 4", &book4);
     change_title(&mut book4);
-    print_book(&book4);
+    print_book("Book 4", &book4);
 }
 
-fn print_book(book: &Book) {
-    println!("Title: {}",book.title);
+fn print_book(name: &str, book: &Book) {
+    println!("{} Title: {}",name,book.title);
 }
 
 fn change_title(book: &mut Book) {
