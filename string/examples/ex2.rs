2dff889babc893d92d6ac13239dec3bd5a652f0a
Michael Angerman <stormasm@gmail.com>
2021-08-08 10:11:26 -0700

    mw

--- string/examples/ex2.rs
+++ string/examples/ex2.rs
@@ -1,10 +1,12 @@
-fn pass_along_str(s: &String) -> String {
+fn pass_along_str(s: &mut String) -> String {
+    let tobuy = String::from("to buy some apples");
+    s.push_str(&tobuy);
     s.to_string()
 }
 
 fn main() -> std::io::Result<()> {
-    let s = "john walked to the store".into();
-    let s1 = pass_along_str(&s);
+    let mut s = "john walked to the store ".into();
+    let s1 = pass_along_str(&mut s);
     println!("{}",s1);
     Ok(())
 }
