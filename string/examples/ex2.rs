e830a7d1f95ee7ddb74031b5dc733f1fbfd80654
Michael Angerman <stormasm@gmail.com>
2021-08-08 10:30:17 -0700

    mw

--- string/examples/ex2.rs
+++ string/examples/ex2.rs
@@ -1,7 +1,7 @@
 /// You only want to pass along str or String
 /// And return only String's
 
-fn pass_along_str(s: &mut String) -> String {
+fn pass_along_string(s: &mut String) -> String {
     let tobuy = String::from("to buy some apples");
     s.push_str(&tobuy);
     s.to_string()
@@ -9,7 +9,7 @@ fn pass_along_str(s: &mut String) -> String {
 
 fn main() -> std::io::Result<()> {
     let mut s = "john walked to the store ".into();
-    let s1 = pass_along_str(&mut s);
+    let s1 = pass_along_string(&mut s);
     println!("{}", s1);
     Ok(())
 }
