5ba5405d032aa983aca38d1ed4621e14c3903b3e
Michael Angerman <stormasm@gmail.com>
2021-08-26 14:25:20 -0700

    update to async

--- reqwest/examples/exa.rs
+++ reqwest/examples/exa.rs
@@ -1,8 +1,29 @@
-use std::collections::HashMap;
+#![deny(warnings)]
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("https://httpbin.org/ip")?
-        .json::<HashMap<String, String>>()?;
-    println!("{:#?}", resp);
+// This is using the `tokio` runtime. You'll need the following dependency:
+//
+// `tokio = { version = "1", features = ["full"] }`
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+
+    let myurl = "https://seekingalpha.com/article/4451692-alibaba-the-mass-exodus";
+    //let myurl = "https://hyper.rs";
+
+    let res = reqwest::get(myurl).await?;
+
+    println!("Status: {}", res.status());
+
+    let body = res.text().await?;
+
+    println!("Body:\n\n{}", body);
+
     Ok(())
 }
+
+// The [cfg(not(target_arch = "wasm32"))] above prevent building the tokio::main function
+// for wasm32 target, because tokio isn't compatible with wasm32.
+// If you aren't building for wasm32, you don't need that line.
+// The two lines below avoid the "'main' function not found" error when building for wasm32 target.
+#[cfg(target_arch = "wasm32")]
+fn main() {}
