3d3f4ea30692c5286a86356e57cfcc37893331b8
Michael Angerman <stormasm@gmail.com>
2021-12-21 16:59:16 -0800

    update main and ex00

--- serde/examples/ex00.rs
+++ serde/examples/ex00.rs
@@ -6,13 +6,16 @@ extern crate serde_json;
 #[derive(Serialize, Deserialize, Debug)]
 struct Contact {
     firstname: String,
-    lastname: String
+    lastname: String,
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let mycontact = Contact { firstname: "rick".to_string(), lastname: "stevens".to_string() };
+    let mycontact = Contact {
+        firstname: "rick".to_string(),
+        lastname: "stevens".to_string(),
+    };
     let serialized = serde_json::to_string(&mycontact).unwrap();
     println!("Serialized: {}", serialized);
 
--- serde/src/main.rs
+++ serde/src/main.rs
@@ -1,18 +1,20 @@
 #[macro_use]
-extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Contact {
     firstname: String,
-    lastname: String
+    lastname: String,
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let mycontact = Contact { firstname: "rick".to_string(), lastname: "stevens".to_string() };
+    let mycontact = Contact {
+        firstname: "rick".to_string(),
+        lastname: "stevens".to_string(),
+    };
     let serialized = serde_json::to_string(&mycontact).unwrap();
     println!("Serialized: {}", serialized);
 
