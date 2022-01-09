31b665bae977e131250a57f7991bded1bef300ec
Michael Angerman <stormasm@gmail.com>
2022-01-09 08:55:24 -0800

    mw

--- sort/examples/ex00.rs
+++ sort/examples/ex00.rs
@@ -1,9 +1,20 @@
+fn f2() {
+    let mut vec: Vec<u32> = vec![45, 3, 6, 2];
+    vec.sort();
+    println!("{:?}", vec)
+}
+
+// This switches around the order in which elements are compared,
+// so that smaller elements appear larger
+// to the sorting function and vice versa.
+
 fn f1() {
     let mut vec: Vec<u32> = vec![45, 3, 6, 2];
     vec.sort_by(|a, b| b.cmp(a));
-    println!("{:?}",vec)
+    println!("{:?}", vec)
 }
 
 fn main() {
-    f1()
+    f1();
+    f2()
 }
