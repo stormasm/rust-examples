42f68e2a4dc35e5f5115398376d34f790cd68c25
Michael Angerman <stormasm@gmail.com>
2022-01-23 09:48:33 -0800

    initial commit of vec directory

---
+++ vec/examples/ex0.rs
@@ -0,0 +1,20 @@
+/*
+*  Check to see if a column exists in a vec of columns
+*/
+
+fn column_exists(input: Vec<String>, columns: Vec<String>) -> bool {
+    for column in columns {
+        if let Some(index) = input.iter().position(|value| *value == column) {
+            return true
+        }
+    }
+    return false
+}
+
+
+fn main() {
+    let input = vec!["red".to_string()];
+    let columns = vec!["red".to_string(),"blue".to_string()];
+    let result = column_exists(input,columns);
+    println!("{:?}",result);
+}
