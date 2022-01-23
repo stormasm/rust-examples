5a9b021808f017c11a3d8288d253a4e6b6bd34f2
Michael Angerman <stormasm@gmail.com>
2022-01-23 12:39:41 -0800

    got things working with do_vecs_match

--- vec/examples/ex0.rs
+++ vec/examples/ex0.rs
@@ -1,14 +1,30 @@
 /*
-*  Check to see if a column does not exist in a vec of columns
+*  Check to see if any of the columns inside the input
+*  does not exist in a vec of columns
 */
 
-fn column_does_not_exist(input: Vec<String>, columns: Vec<String>) -> bool {
+fn do_vecs_match<T: PartialEq>(a: &Vec<T>, b: &Vec<T>) -> bool {
+    let matching = a.iter().zip(b.iter()).filter(|&(a, b)| a == b).count();
+    matching == a.len() && matching == b.len()
+}
+
+fn column_does_not_exist(inputs: Vec<String>, columns: Vec<String>) -> bool {
+    if do_vecs_match(&inputs, &columns) {
+        return false;
+    }
+
     for column in columns {
-        if let Some(_index) = input.iter().position(|value| *value == column) {
-            return false;
+        println!("column = {:?}", column);
+        for input in &inputs {
+            println!("input = {:?}", input);
+            if *input != column {
+                return true;
+            } else {
+                break;
+            }
         }
     }
-    true
+    false
 }
 
 /*
@@ -25,12 +41,12 @@ fn column_exists(input: Vec<String>, columns: Vec<String>) -> bool {
 }
 
 fn main() {
-    let input1 = vec!["red".to_string(), "white".to_string()];
+    let input1 = vec!["blue".to_string(), "yellow".to_string()];
     let columns1 = vec!["green".to_string(), "blue".to_string()];
     let result = column_exists(input1, columns1);
     println!("Exists = {:?}", result);
 
-    let input2 = vec!["red".to_string(), "white".to_string()];
+    let input2 = vec!["orange".to_string(), "white".to_string()];
     let columns2 = vec!["green".to_string(), "blue".to_string()];
     let result = column_does_not_exist(input2, columns2);
     println!("Does not exist = {:?}", result);
