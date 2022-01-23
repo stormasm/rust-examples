90295612e3b0abc377f8be024d70371126fd14be
Michael Angerman <stormasm@gmail.com>
2022-01-23 11:24:46 -0800

    mw

--- vec/examples/ex0.rs
+++ vec/examples/ex0.rs
@@ -1,4 +1,17 @@
 /*
+*  Check to see if a column does not exist in a vec of columns
+*/
+
+fn column_does_not_exist(input: Vec<String>, columns: Vec<String>) -> bool {
+    for column in columns {
+        if let Some(_index) = input.iter().position(|value| *value == column) {
+            return false;
+        }
+    }
+    true
+}
+
+/*
 *  Check to see if a column exists in a vec of columns
 */
 
@@ -12,8 +25,13 @@ fn column_exists(input: Vec<String>, columns: Vec<String>) -> bool {
 }
 
 fn main() {
-    let input = vec!["red".to_string(), "white".to_string()];
-    let columns = vec!["green".to_string(), "blue".to_string()];
-    let result = column_exists(input, columns);
-    println!("{:?}", result);
+    let input1 = vec!["red".to_string(), "white".to_string()];
+    let columns1 = vec!["green".to_string(), "blue".to_string()];
+    let result = column_exists(input1, columns1);
+    println!("Exists = {:?}", result);
+
+    let input2 = vec!["red".to_string(), "white".to_string()];
+    let columns2 = vec!["green".to_string(), "blue".to_string()];
+    let result = column_does_not_exist(input2, columns2);
+    println!("Does not exist = {:?}", result);
 }
