7b4fdc492e044c197d465efc821049f019409c4f
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-02-02 16:12:13 -0800

    initial commit of ex2

---
+++ vec/examples/ex2.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let v1 = Value::Int { val: 4 };
+    let v2 = Value::Int { val: 40 };
+    let v3 = Value::Int { val: 10 };
+
+    let mut vec = vec![v1, v2, v3];
+    vec.sort_by(|a, b| process(a, b));
+    println!("{:?}", vec);
+}
+
+pub fn process(left: &Value, right: &Value) -> std::cmp::Ordering {
+    println!("{:?} {:?}", left, right);
+    std::cmp::Ordering::Less
+}
+
+#[derive(Debug)]
+pub enum Value {
+    Bool { val: bool },
+    Int { val: i64 },
+    Float { val: f64 },
+    String { val: String },
+}
