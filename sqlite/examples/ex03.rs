730890604adbc73addb4cf28b9b9c9db834323b4
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-02 21:30:59 -0700

    mw

--- sqlite/examples/ex03.rs
+++ sqlite/examples/ex03.rs
@@ -1,21 +1,9 @@
-use rusqlite::{Connection, Result};
-
-//use rusqlite::vtab::{VTabConnection, VTabCursor, Values};
-
-/*
-use rusqlite::vtab::{
-    dequote, escape_double_quote, parse_boolean, read_only_module, Context, CreateVTab, IndexInfo,
-    VTab, VTabConnection, VTabCursor, Values,
-};
-*/
-//use crate::{Connection, Error, Result};
 use fallible_iterator::FallibleIterator;
 use rusqlite::vtab::csvtab::load_module;
+use rusqlite::{Connection, Result};
 
 fn main() -> Result<()> {
     let db = Connection::open("mydb3.db")?;
-
-    //let db = Connection::open_in_memory()?;
     load_module(&db)?;
     db.execute_batch("CREATE VIRTUAL TABLE vtab USING csv(filename='./csv/test.csv', header=yes)")?;
 
@@ -30,7 +18,7 @@ fn main() -> Result<()> {
         let sum = ids?.iter().sum::<i32>();
         assert_eq!(sum, 15);
     }
-    //db.execute_batch("DROP TABLE vtab");
+    db.execute_batch("DROP TABLE vtab")
 
-    Ok(())
+    //Ok(())
 }
