d25678f37fde5e8b5de1e4332726bb9cff3b65f3
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-02 21:38:20 -0700

    mw

--- sqlite/examples/ex04.rs
+++ sqlite/examples/ex04.rs
@@ -5,20 +5,11 @@ use rusqlite::{Connection, Result};
 fn main() -> Result<()> {
     let db = Connection::open("mydb3.db")?;
     load_module(&db)?;
-    db.execute_batch("CREATE VIRTUAL TABLE vtab USING csv(filename='./csv/test.csv', header=yes)")?;
-
     let mut s = db.prepare("SELECT rowid, * FROM vtab")?;
-
     let headers = s.column_names();
     assert_eq!(vec!["rowid", "colA", "colB", "colC"], headers);
-
     let ids: Result<Vec<i32>> = s.query([])?.map(|row| row.get::<_, i32>(0)).collect();
     let sum = ids?.iter().sum::<i32>();
     assert_eq!(sum, 15);
-
-    // remove the comment from the next line
-    // when not using in concert with ex04
-    // db.execute_batch("DROP TABLE vtab")
-
     Ok(())
 }
