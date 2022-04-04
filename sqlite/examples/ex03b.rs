5e912b341e7140043380ab33721c02551cbcd2f0
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-04 13:40:06 -0700

    mw

--- sqlite/examples/ex03b.rs
+++ sqlite/examples/ex03b.rs
@@ -1,30 +1,24 @@
-use std::env::args;
-
 use rusqlite::vtab::csvtab::load_module;
 use rusqlite::{Connection, Result};
+use std::env::args;
 
 fn main() -> Result<()> {
     let args: Vec<String> = args().collect();
 
     if args.len() != 3 {
-        println!("Please enter a database name and a csv file name");
+        println!("Please enter a database name and a csv file name like this:");
+        println!("people1.db ./csv/people1.csv");
     } else {
         let dbname = &args[1];
         let myfilename = &args[2];
-
-        // let db = Connection::open("mydb3a.db")?;
         let db = Connection::open(dbname)?;
-
         load_module(&db)?;
-
-        //    let s = format!("{}", foo)
 
         let s = format!(
             "CREATE VIRTUAL TABLE vtab USING csv(filename={}, header=yes)",
             myfilename
         );
 
-        //db.execute_batch("CREATE VIRTUAL TABLE vtab USING csv(filename='./csv/test.csv', header=yes)")?;
         let _x = db.execute_batch(&s);
     }
 
