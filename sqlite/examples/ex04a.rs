15cd44b10e2a8c723b796c665460405447d37547
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-03 20:46:17 -0700

    mw

---
+++ sqlite/examples/ex04a.rs
@@ -0,0 +1,18 @@
+use fallible_iterator::FallibleIterator;
+use rusqlite::vtab::csvtab::load_module;
+use rusqlite::{Connection, Result};
+
+fn main() -> Result<()> {
+    let db = Connection::open("people.db")?;
+    load_module(&db)?;
+    let mut s = db.prepare("SELECT rowid, * FROM vtab")?;
+    let headers = s.column_names();
+    println!("{:?}", headers);
+    //    assert_eq!(vec!["rowid", "colA", "colB", "colC"], headers);
+    let ids: Result<Vec<i32>> = s.query([])?.map(|row| row.get::<_, i32>(0)).collect();
+    println!("{:?}", ids);
+    //    let sum = ids?.iter().sum::<i32>();
+    //    assert_eq!(sum, 15);
+
+    Ok(())
+}
