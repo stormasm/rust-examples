e7648a6ba383a309d6c1dcf3f0b18b36b0c0fc6c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-11 11:04:52 -0700

    mw

--- sqlite/examples/ex05a.rs
+++ sqlite/examples/ex05a.rs
@@ -1,4 +1,3 @@
-use rusqlite::vtab::csvtab::load_module;
 use rusqlite::{Connection, Result};
 
 fn main() -> Result<()> {
@@ -10,7 +9,7 @@ fn main() -> Result<()> {
     //    let mut out = Vec::new();
     let mut table_stmt = conn.prepare(&format!("select * from [{}]", table_name))?;
     let mut table_rows = table_stmt.query([])?;
-    while let Some(table_row) = table_rows.next()? {
+    while let Some(_table_row) = table_rows.next()? {
         println!("hola");
         //println!("{:?}",table_row);
         // out.push(convert_sqlite_row_to_nu_value(table_row, tag.clone()))
