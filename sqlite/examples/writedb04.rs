8a2fce2b249cabf514239552c9a3a8726694d928
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-12 09:20:06 -0700

    mw

--- sqlite/examples/writedb04.rs
+++ sqlite/examples/writedb04.rs
@@ -6,16 +6,18 @@ struct Person {
     id: i32,
     name: String,
     data: Option<Vec<u8>>,
+    grade: f64,
 }
 
 fn main() -> Result<()> {
-    let conn = Connection::open("person03.db")?;
+    let conn = Connection::open("person04.db")?;
 
     conn.execute(
         "CREATE TABLE person (
                   id              INTEGER PRIMARY KEY,
                   name            TEXT NOT NULL,
-                  data            BLOB
+                  data            BLOB,
+                  grade           REAL
                   )",
         [],
     )?;
@@ -24,41 +26,45 @@ fn main() -> Result<()> {
         id: 0,
         name: "Steven".to_string(),
         data: None,
+        grade: 3.4,
     };
 
     let me1 = Person {
         id: 1,
         name: "Pete".to_string(),
         data: Some(r#"a"#.as_bytes().to_vec()),
+        grade: 4.1,
     };
 
     let me2 = Person {
         id: 2,
         name: "Bill".to_string(),
         data: None,
+        grade: 5.2,
     };
 
     conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        params![me0.name, me0.data],
+        "INSERT INTO person (name, data, grade) VALUES (?1, ?2, ?3)",
+        params![me0.name, me0.data, me0.grade],
     )?;
 
     conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        params![me1.name, me1.data],
+        "INSERT INTO person (name, data, grade) VALUES (?1, ?2, ?3)",
+        params![me1.name, me1.data, me1.grade],
     )?;
 
     conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        params![me2.name, me2.data],
+        "INSERT INTO person (name, data, grade) VALUES (?1, ?2, $3)",
+        params![me2.name, me2.data, me2.grade],
     )?;
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    let mut stmt = conn.prepare("SELECT id, name, data, grade FROM person")?;
     let person_iter = stmt.query_map([], |row| {
         Ok(Person {
             id: row.get(0)?,
             name: row.get(1)?,
             data: row.get(2)?,
+            grade: row.get(3)?,
         })
     })?;
 
