5b66c3a90649f31d340aa06cfc7f1e42ac97e95e
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-11 20:09:27 -0700

    mw

--- sqlite/examples/ex05c.rs
+++ sqlite/examples/ex05c.rs
@@ -20,9 +20,23 @@ fn convert_sqlite_value_to_nu_value(value: ValueRef) -> Value {
             println!("got Real {:?}", f);
             Value::Float { val: f, span: span }
         }
-        ValueRef::Text(s) => {
-            println!("got Text {:?}", s);
-            Value::Nothing { span }
+        ValueRef::Text(buf) => {
+            //            println!("got Text {:?}", buf);
+            //            let value = s.into_value(span);
+
+            //            Value::Nothing { span }
+
+            let s = match std::str::from_utf8(buf) {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+
+            println!("got Text {:?}", s.to_string());
+
+            Value::String {
+                val: s.to_string(),
+                span: span,
+            }
         }
         ValueRef::Blob(u) => {
             println!("got Blob {:?}", u);
