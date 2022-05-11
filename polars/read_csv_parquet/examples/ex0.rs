8f50214543b71d3d508f2b059d34cfdd5a48b381
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-05-11 10:36:02 -0700

    mw

--- polars/read_csv_parquet/examples/ex0.rs
+++ polars/read_csv_parquet/examples/ex0.rs
@@ -2,16 +2,27 @@
 //!use polars::prelude::{CsvEncoding, CsvReader, JsonReader, ParquetReader, SerReader};
 //!use polars::prelude::*;
 
-use polars::prelude::{CsvReader, DataFrame, Result, SerReader};
+use polars::prelude::{CsvReader, CsvWriter, DataFrame, Result, SerReader, SerWriter};
+use std::fs::File;
 
-fn example() -> Result<DataFrame> {
+fn exread() -> Result<DataFrame> {
     // always prefer `from_path` as that is fastest.
     CsvReader::from_path("../datasets/foods1.csv")?
         .has_header(true)
         .finish()
 }
 
+fn exwrite(df: &mut DataFrame) -> Result<()> {
+    let mut file = File::create("example.csv").expect("could not create file");
+
+    CsvWriter::new(&mut file)
+        .has_header(true)
+        .with_delimiter(b',')
+        .finish(df)
+}
+
 fn main() {
-    let df = example();
+    let df = exread();
     println!("{:?}", df);
+    exwrite(&mut df.unwrap());
 }
