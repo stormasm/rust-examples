cda62ee93a7f744765fb7bd6a74104948a5aded6
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-05-11 09:44:11 -0700

    starting to add in some examples

---
+++ polars/read_csv_parquet/examples/ex0.rs
@@ -0,0 +1,14 @@
+//! ## Read a csv file to a DataFrame
+//!
+//!use polars::prelude::{CsvEncoding, CsvReader, JsonReader, ParquetReader, SerReader};
+//use polars::prelude::{CsvReader, DataFrame};
+use polars::prelude::*;
+
+fn example() -> Result<DataFrame> {
+    // always prefer `from_path` as that is fastest.
+    CsvReader::from_path("../../datasets/foods1.csv")?.has_header(true).finish()
+}
+
+fn main() {
+    let _ = example();
+}
