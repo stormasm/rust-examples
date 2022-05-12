fc299418867b8d2b2bd92883ef0002fe068d0bda
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-05-12 16:53:15 -0700

    mw

--- polars/read_csv_parquet/examples/read.rs
+++ polars/read_csv_parquet/examples/read.rs
@@ -3,7 +3,6 @@ use std::env;
 use std::fs::File;
 
 fn read(filename: &str) -> Result<DataFrame> {
-    //  let r = File::open("../datasets/foods1.parquet").unwrap();
     let r = File::open(filename).unwrap();
     let reader = ParquetReader::new(r);
     reader.finish()
@@ -11,6 +10,15 @@ fn read(filename: &str) -> Result<DataFrame> {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    println!("{:?}", args.len());
+    if args.len() == 1 {
+        println!(
+            "{}",
+            "You must enter a filename like ../datasets/foods1.parquet"
+        );
+        return;
+    }
+
     let filename = &args[1];
     let df = read(&filename).unwrap();
     println!("{:?}", df);
