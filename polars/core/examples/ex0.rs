776d1d0906ff425d8c3ede988d75be97c097c989
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-05-13 15:06:15 -0700

    mw

--- polars/core/examples/ex0.rs
+++ polars/core/examples/ex0.rs
@@ -1,29 +1,31 @@
 use polars::df;
 use polars::prelude::{DataFrame, NamedFrom, Result, Series};
 
+fn test1() {
+    let df = DataFrame::default();
+    assert!(df.is_empty());
+    println!("{:?}", df);
+}
+
+fn test2() {
+    let s1 = Series::new("Fruit", &["Apple", "Apple", "Pear"]);
+    let s2 = Series::new("Color", &["Red", "Yellow", "Green"]);
+
+    let df: Result<DataFrame> = DataFrame::new(vec![s1, s2]);
+    println!("{:?}", df.unwrap());
+}
+
 fn test3() {
-    let dfd = df!("Fruit" => &["Apple", "Apple", "Pear"],
+    let df: Result<DataFrame> = df!("Fruit" => &["Apple", "Apple", "Pear"],
               "Color" => &["Red", "Yellow", "Green"]);
 
-    let x = dfd.unwrap();
+    let x = df.unwrap();
     println!("{:?}", x[0]);
     println!("{:?}", x["Color"])
 }
 
 fn main() {
-    let dfa = DataFrame::default();
-    assert!(dfa.is_empty());
-    println!("{:?}", dfa);
-
-    let s1 = Series::new("Fruit", &["Apple", "Apple", "Pear"]);
-    let s2 = Series::new("Color", &["Red", "Yellow", "Green"]);
-
-    let dfb: Result<DataFrame> = DataFrame::new(vec![s1, s2]);
-    println!("{:?}", dfb.unwrap());
-
-    let dfc: Result<DataFrame> = df!("Fruit" => &["Apple", "Apple", "Pear"],
-                                    "Color" => &["Red", "Yellow", "Green"]);
-    println!("{:?}", dfc.unwrap());
-
+    test1();
+    test2();
     test3();
 }
