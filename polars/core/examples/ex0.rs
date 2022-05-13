d7c1d85e046957a4054eacfa13cc0e042a06eee2
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-05-13 12:29:17 -0700

    mw

--- polars/core/examples/ex0.rs
+++ polars/core/examples/ex0.rs
@@ -1,7 +1,13 @@
-use polars::prelude::DataFrame;
+use polars::prelude::{DataFrame, NamedFrom, Result, Series};
 
 fn main() {
-    let df = DataFrame::default();
-    assert!(df.is_empty());
-    println!("{:?}", df);
+    let dfa = DataFrame::default();
+    assert!(dfa.is_empty());
+    println!("{:?}", dfa);
+
+    let s1 = Series::new("Fruit", &["Apple", "Apple", "Pear"]);
+    let s2 = Series::new("Color", &["Red", "Yellow", "Green"]);
+
+    let dfb: Result<DataFrame> = DataFrame::new(vec![s1, s2]);
+    println!("{:?}", dfb.unwrap());
 }
