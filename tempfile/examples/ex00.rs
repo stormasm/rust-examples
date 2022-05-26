0058e091c757d25950c57fe389f3f09657d3816b
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-05-26 08:28:00 -0700

    mw

--- tempfile/examples/ex00.rs
+++ tempfile/examples/ex00.rs
@@ -4,6 +4,7 @@ use tempfile::tempfile;
 fn test1() -> Result<()> {
     // Create a file inside of `std::env::temp_dir()`.
     let mut file = tempfile()?;
+    println!("{:?}", file);
     writeln!(file, "Ralph was here. Then he left.")?;
     Ok(())
 }
