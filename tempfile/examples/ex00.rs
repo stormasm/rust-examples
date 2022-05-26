b1b2f5a73b7583134272ad76c9fe011370033a36
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-05-26 08:18:29 -0700

    mw

--- tempfile/examples/ex00.rs
+++ tempfile/examples/ex00.rs
@@ -1,20 +1,3 @@
-/*
-use tempfile::tempfile;
-use std::io::{self, Write};
-
-use std::io::{Write, Result};
-
-fn main() -> Result<()> {
-
-
-// fn main() {
-// Create a file inside of `std::env::temp_dir()`.
-let mut file = tempfile();
-
-writeln!(file, "Ralph was here. Then he left.")?;
-}
-*/
-
 use std::io::{Result, Write};
 use tempfile::tempfile;
 
@@ -27,14 +10,5 @@ fn test1() -> Result<()> {
 
 fn main() -> Result<()> {
     let _ = test1();
-
-    /*
-        let mut w = Vec::new();
-        writeln!(&mut w)?;
-        writeln!(&mut w, "test")?;
-        writeln!(&mut w, "formatted {}", "arguments")?;
-
-        assert_eq!(&w[..], "\ntest\nformatted arguments\n".as_bytes());
-    */
     Ok(())
 }
