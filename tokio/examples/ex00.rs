a0495085edbc06d5ab4380c8902dadfb67e997d3
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-06-21 09:35:35 -0700

    add in the example tinydb

--- tokio/examples/ex00.rs
+++ tokio/examples/ex00.rs
@@ -1,16 +1,16 @@
-use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    
+
     loop {
         let (mut socket, _) = listener.accept().await?;
-        
+
         tokio::spawn(async move {
             let mut buf = [0; 1024];
-            
+
             // In a loop, read data from the socket and write the data back.
             loop {
                 let n = match socket.read(&mut buf).await {
@@ -22,7 +22,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         return;
                     }
                 };
-                
+
                 // Write the data back
                 if let Err(e) = socket.write_all(&buf[0..n]).await {
                     eprintln!("failed to write to socket; err = {:?}", e);
