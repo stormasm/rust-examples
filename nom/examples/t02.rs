c0bc0dbf885d708ba28108d4d0322b0fcfff980e
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-21 15:20:34 -0700

    mw

--- nom/examples/t02.rs
+++ nom/examples/t02.rs
@@ -1,10 +1,11 @@
 use nom::{bytes::complete::tag, IResult};
 
-fn foo(s: &str) -> IResult<&str, &str> {
-    tag("Error running remote query: ")(s)
+fn part00(s: &str) -> IResult<&str, &str> {
+    let remote_query: &'static str = "Error running remote query: ";
+    tag(remote_query)(s)
 }
 
 fn main() {
-    let result = foo("Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning:");
+    let result = part00("Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning:");
     println!("{:?}", result);
 }
