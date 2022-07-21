7c7849e38bec325a442a2b39d996ed8d1dfdc1b8
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-21 16:54:29 -0700

    mw

--- nom/examples/t03.rs
+++ nom/examples/t03.rs
@@ -6,11 +6,11 @@ use nom::{bytes::complete::take_until, IResult};
 
 fn remove00(s: &str) -> IResult<&str, &str> {
     // let remote_query: &'static str = "metadata: ";
-    take_until(", metadata: ")(s)
+    take_until(", details: ")(s)
 }
 
 fn main() {
-    let data: &'static str = "details: [], metadata: MetadataMap { headers: {\"content-type\": \"application/grpc\", \"date\": \"Wed, 20 Jul 2022 19:08:52 GMT\", \"content-length\": \"0\"} }";
+    let data: &'static str = "during planning: 'public.iox.h2o_xtemperature' not found\", details: [], metadata: MetadataMap { headers: {\"content-type\": \"application/grpc\", \"date\": \"Wed, 20 Jul 2022 19:08:52 GMT\", \"content-length\": \"0\"} }";
     let result = remove00(data).unwrap().1;
     println!("{:?}", result);
 }
