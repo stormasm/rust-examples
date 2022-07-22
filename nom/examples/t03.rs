6a92392a880ea55984fb17fdb5c3b74d8bf72b7c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-22 16:27:53 -0700

    mw

--- nom/examples/t03.rs
+++ nom/examples/t03.rs
@@ -10,16 +10,25 @@ fn remove_details(s: &str) -> IResult<&str, &str> {
 }
 
 fn get_message(s: &str) -> IResult<&str, &str> {
-    let delete: &'static str = ", message: ";
-    take_until(delete)(s)
+    let msg: &'static str = ", message: ";
+    take_until(msg)(s)
+}
+
+fn get_header(s: &str) -> IResult<&str, &str> {
+    let header: &'static str = "status: ";
+    take_until(header)(s)
 }
 
 fn main() {
     // let data: &'static str = "during planning: 'public.iox.h2o_xtemperature' not found\", details: [], metadata: MetadataMap { headers: {\"content-type\": \"application/grpc\", \"date\": \"Wed, 20 Jul 2022 19:08:52 GMT\", \"content-length\": \"0\"} }";
     let data: &'static str = "Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning: 'public.iox.h2o_xtemperature' not found\", details: [], metadata: MetadataMap { headers: {\"content-type\": \"application/grpc\", \"date\": \"Wed, 20 Jul 2022 19:08:52 GMT\", \"content-length\": \"0\"} }";
     let details = remove_details(data).unwrap().1;
-    let result = get_message(details);
-    println!("result: {:?}", result);
+    let (message, remainder) = get_message(details).unwrap();
+    println!("{:?}\n{:?}", &remainder, &message);
+    let (status, header) = get_header(&remainder).unwrap();
+    println!("{:?}\n{:?}", &header, &status);
+
+    //println!("result: {:?}", result);
     /*
     let message = result.unwrap().0;
     let status = result.unwrap().1;
