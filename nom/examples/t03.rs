fc88d97164d7e68d0fc72965ae0dd5cfe287145b
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-22 17:22:42 -0700

    mw

--- nom/examples/t03.rs
+++ nom/examples/t03.rs
@@ -1,7 +1,3 @@
-/*
-Leave everything in the string except a certain part of the string
-*/
-
 use nom::{bytes::complete::take_until, IResult};
 
 fn remove_details(s: &str) -> IResult<&str, &str> {
@@ -28,7 +24,6 @@ fn remove_char1_from_string(s: &String) -> String {
 }
 
 fn main() {
-    // let data: &'static str = "during planning: 'public.iox.h2o_xtemperature' not found\", details: [], metadata: MetadataMap { headers: {\"content-type\": \"application/grpc\", \"date\": \"Wed, 20 Jul 2022 19:08:52 GMT\", \"content-length\": \"0\"} }";
     let data: &'static str = "Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning: 'public.iox.h2o_xtemperature' not found\", details: [], metadata: MetadataMap { headers: {\"content-type\": \"application/grpc\", \"date\": \"Wed, 20 Jul 2022 19:08:52 GMT\", \"content-length\": \"0\"} }";
     let details = remove_details(data).unwrap().1;
     let (message, remainder) = get_message(details).unwrap();
@@ -38,8 +33,8 @@ fn main() {
     let header1 = remove_char1_from_string(&header.to_string());
     println!("{:?}", header1.trim());
 
-    //println!("{:?}", &header);
     println!("{:?}", &status);
+
     let msg1 = remove_char_from_string(&message.to_string());
     println!("{:?}", msg1.trim());
 }
