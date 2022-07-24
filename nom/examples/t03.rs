16e9a44beb1a423ce14df8cfac0bec01e04ded8e
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-23 21:09:03 -0700

    cc

--- nom/examples/t03.rs
+++ nom/examples/t03.rs
@@ -15,11 +15,11 @@ fn get_header(s: &str) -> IResult<&str, &str> {
     take_until(header)(s)
 }
 
-fn remove_char_from_string(s: &String) -> String {
+fn remove_slash_from_string(s: &String) -> String {
     s.replace(&['(', ')', ',', '\"', ';', '\''][..], "")
 }
 
-fn remove_char1_from_string(s: &String) -> String {
+fn remove_colon_from_string(s: &String) -> String {
     s.replace(&[':'][..], "")
 }
 
@@ -30,11 +30,11 @@ fn main() {
     //println!("{:?}\n{:?}", &remainder, &message);
     let (status, header) = get_header(&remainder).unwrap();
 
-    let header1 = remove_char1_from_string(&header.to_string());
+    let header1 = remove_colon_from_string(&header.to_string());
     println!("{:?}", header1.trim());
 
     println!("{:?}", &status);
 
-    let msg1 = remove_char_from_string(&message.to_string());
+    let msg1 = remove_slash_from_string(&message.to_string());
     println!("{:?}", msg1.trim());
 }
