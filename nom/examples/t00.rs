b13819fbec57ac8af346381aa945d4d87e622fa7
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-25 17:51:29 -0700

    mw

--- nom/examples/t00.rs
+++ nom/examples/t00.rs
@@ -1,13 +1,13 @@
 use nom::{bytes::complete::is_a, IResult};
 
-fn error_check(s: &str) -> IResult<&str, &str> {
+fn is_a_error(s: &str) -> IResult<&str, &str> {
     let remote_query: &'static str = "Error";
     is_a(remote_query)(s)
 }
 
 fn main() {
-    //let result = error_check("Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning:");
-    let result = error_check("jack");
+    let result = is_a_error("Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning:");
+    //let result = is_a_error("jack");
 
     let mybool = match result.is_err() {
         true => true,
