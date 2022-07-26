0b9aaa3cf4f7a6e9669f9a032299d0d57f226fd7
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-26 16:05:00 -0700

    update code documentation to elucidate how the code works

--- nom/examples/t00.rs
+++ nom/examples/t00.rs
@@ -1,18 +1,27 @@
 use nom::{bytes::complete::is_a, IResult};
 
-fn is_a_error(s: &str) -> IResult<&str, &str> {
+pub fn is_a_error(s: &str) -> IResult<&str, &str> {
     let remote_query: &'static str = "Error";
     is_a(remote_query)(s)
 }
 
-fn main() {
-    let result = is_a_error("Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning:");
-    //let result = is_a_error("jack");
+// This returns true if there is not the word Error in the string,
+// meaning that an error was thrown by nom because it can not find the Error string
+// This returns false if the string has the word Error in it
+pub fn error_check(s: &str) -> bool {
+    let result = is_a_error(s);
+    println!("error_check result 2 = {:?}", result);
 
     let mybool = match result.is_err() {
         true => true,
         false => false,
     };
 
-    println!("{:?}", mybool);
+    return mybool;
+}
+
+fn main() {
+    //let result = error_check("Error running remote query: status: InvalidArgument, message: \"Error while planning query: Error during planning:");
+    let result = error_check("jack");
+    println!("{:?}", result);
 }
