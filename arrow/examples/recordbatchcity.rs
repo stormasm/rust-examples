804684553660a9225014f81987b1d22542114d44
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-09-19 12:44:01 -0700

    mw

--- arrow/examples/recordbatchcity.rs
+++ arrow/examples/recordbatchcity.rs
@@ -10,14 +10,6 @@ fn main() -> arrow::error::Result<()> {
     let id0 = Int32Array::from(vec![1, 2, 3, 4, 5]);
     let id1 = Int32Array::from(vec![6, 7, 8, 9, 10]);
 
-    let batch = RecordBatch::try_new(
-        Arc::new(Schema::new(vec![
-            Field::new("id0", DataType::Int32, false),
-            Field::new("id1", DataType::Int32, false),
-        ])),
-        vec![Arc::new(id0), Arc::new(id1)],
-    )?;
-
     let city = Arc::new(StringArray::from(vec![
         Some("santafe"),
         Some("socorro"),
@@ -28,7 +20,7 @@ fn main() -> arrow::error::Result<()> {
     let coop = Arc::new(BooleanArray::from(vec![true, false, false, true]));
     let pop = Arc::new(Int32Array::from(vec![121, 28, 125, 631]));
 
-    let struct_array = StructArray::from(vec![
+    let _struct_array = StructArray::from(vec![
         (
             Field::new("city", DataType::Utf8, false),
             city.clone() as ArrayRef,
@@ -43,7 +35,16 @@ fn main() -> arrow::error::Result<()> {
         ),
     ]);
 
-    println!("{:?}", struct_array);
+    // println!("{:?}", struct_array);
+
+    let batch = RecordBatch::try_new(
+        Arc::new(Schema::new(vec![
+            Field::new("id0", DataType::Int32, false),
+            Field::new("id1", DataType::Int32, false),
+        ])),
+        vec![Arc::new(id0), Arc::new(id1)],
+    )?;
+
     print_batches(&[batch.clone()]).unwrap();
 
     Ok(())
