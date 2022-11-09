4b8b79c114cd3a82f2fe20a895f482396c3e35df
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-11-09 09:10:39 -0800

    mw

--- fileio/examples/env_currentdir.rs
+++ fileio/examples/env_currentdir.rs
@@ -1,5 +1,9 @@
 use std::env;
 
+// pub fn current_dir() -> Result<PathBuf>
+// Returns the current working directory as a PathBuf.
+// https://doc.rust-lang.org/std/env/fn.current_dir.html
+
 fn main() {
     let dir = env::current_dir().unwrap();
     println!("{:?}", dir);
