bcd6f39274aade82744a97f8fab43ad3d3f9866b
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-01-22 11:36:39 -0800

    mw

---
+++ datafusion/examples/df00.rs
@@ -0,0 +1,9 @@
+use datafusion::error::Result;
+use datafusion::prelude::*;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let ctx = SessionContext::new();
+    let df = ctx.read_csv("data/red0.csv", CsvReadOptions::new()).await?;
+    df.collect().await?;
+    Ok(())
+}
