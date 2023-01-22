ce56ba3356d95b607cfc6f87fd6d9b3fda70c5ad
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-01-22 11:38:29 -0800

    mw

--- datafusion/examples/df00.rs
+++ datafusion/examples/df00.rs
@@ -5,5 +5,7 @@ async fn main() -> Result<()> {
     let ctx = SessionContext::new();
     let df = ctx.read_csv("data/red0.csv", CsvReadOptions::new()).await?;
     df.collect().await?;
+    // print the results
+    // df.show().await?;
     Ok(())
 }
