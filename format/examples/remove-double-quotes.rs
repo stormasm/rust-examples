d1c883b5174e33635b7af5711c6e4d0df867e765
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-06-02 08:04:56 -0700

    initial commit of remove double quotes

---
+++ format/examples/remove-double-quotes.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let vec1 = vec!["1", "john ran to the store", "1993-10-29", "34.56"];
+    //let mut vec2 = Vec::new();
+
+    for i in vec1.iter() {
+        println!("{}", i);
+    }
+}
