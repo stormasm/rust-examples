58bb0394834f2b1e8991ae81ea8f39a198f0511c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-11-29 12:34:53 -0800

    initial commit of ex00.rs

---
+++ hashmap/examples/ex00.rs
@@ -0,0 +1,41 @@
+/*
+* This code started out in notrace/main.rs
+*/
+
+use std::env;
+use std::process;
+use std::string::String;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use std::io;
+use std::io::BufRead;
+use std::io::BufWriter;
+use std::io::Write;
+
+fn check_log_statement(line: String) -> bool {
+    line.contains("trace")
+}
+
+fn read_file_to_buffer(filename: String) {
+    let f = File::open(filename).unwrap();
+    let file = BufReader::new(&f);
+    let mut writer = BufWriter::new(io::stdout());
+    for (_num, line) in file.lines().enumerate() {
+        let l = line.unwrap();
+        if !check_log_statement(l.clone()) {
+            writeln!(writer, "{}", l).unwrap();
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("You need to enter a filename");
+        process::exit(1);
+    }
+    let filename = &args[1];
+    let _contents = read_file_to_buffer(filename.to_string());
+}
