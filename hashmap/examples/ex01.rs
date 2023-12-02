4d4137a26d359d6f4a5737f30bf032e7b4900b23
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-12-02 10:21:54 -0800

    ex01 has the same results as ex00 but this file enables passing the map along in the program

---
+++ hashmap/examples/ex01.rs
@@ -0,0 +1,52 @@
+/*
+* This code is the same as ex00 but with a pointer to hashmap
+*/
+
+use std::collections::HashMap;
+
+use std::env;
+use std::process;
+use std::string::String;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use std::io::BufRead;
+
+fn read_file_to_buffer(map: &mut HashMap<String, i32>, filename: String) {
+    let f = File::open(filename).unwrap();
+    let file = BufReader::new(&f);
+    for (_num, line) in file.lines().enumerate() {
+        let l = line.unwrap();
+
+        match map.get(&l) {
+            Some(count) => {
+                map.insert(l.to_string(), count + 1);
+            }
+            None => {
+                map.insert(l.to_string(), 1);
+            }
+        }
+    }
+    //print(map)
+}
+
+fn print(map: HashMap<String, i32>) {
+    println!("Occurences..");
+
+    for (key, value) in map.iter() {
+        println!("{key}: {value}");
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("You need to enter a filename");
+        process::exit(1);
+    }
+    let filename = &args[1];
+    let mut map = HashMap::new();
+    let _contents = read_file_to_buffer(&mut map, filename.to_string());
+    print(map);
+}
